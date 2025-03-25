f10bafc60a6f6b7c95a8411d4ec879fe91645ad0
Vilem Obratil <vobratil@redhat.com>
2025-03-25 14:04:04 +0100

    Fixed the formatting with format:fix.

--- tests/api/features/relationship-generated-from.ts
+++ tests/api/features/relationship-generated-from.ts
@@ -7,11 +7,13 @@ const cdxDocumentId = "urn:uuid:a4f16b62-fea9-42c1-8365-d72d3cef37d1/1";
 
 const spdxSourcePurl = "pkg:rpm/redhat/openssl@3.0.7-18.el9_2?arch=src";
 const spdxBinaryPurl = "pkg:rpm/redhat/openssl@3.0.7-18.el9_2?arch=x86_64";
-const spdxDocumentId = "https://www.redhat.com/openssl-3.0.7-18.el9_2.spdx.json";
+const spdxDocumentId =
+  "https://www.redhat.com/openssl-3.0.7-18.el9_2.spdx.json";
 
 // Effectively tests TC-2050 / TC-2051 - Denote src to binary rpm relationship.
-test("Generated from / CDX / Source has descendants that include binaries / Get with pURL", async ({ axios }) => {
-
+test("Generated from / CDX / Source has descendants that include binaries / Get with pURL", async ({
+  axios,
+}) => {
   const urlEncodedSourcePurl = encodeURIComponent(cdxSourcePurl);
 
   const vanillaResponse = await axios.get(
@@ -26,16 +28,17 @@ test("Generated from / CDX / Source has descendants that include binaries / Get 
         descendants: expect.arrayContaining([
           expect.objectContaining({
             relationship: "generates",
-            purl: expect.arrayContaining([cdxBinaryPurl])
-          })
-        ])
+            purl: expect.arrayContaining([cdxBinaryPurl]),
+          }),
+        ]),
       }),
     ])
   );
 });
 
-test("Generated from / CDX / Binary has ancestors that include the source / Get with pURL", async ({ axios }) => {
-
+test("Generated from / CDX / Binary has ancestors that include the source / Get with pURL", async ({
+  axios,
+}) => {
   const urlEncodedBinaryPurl = encodeURIComponent(cdxBinaryPurl);
 
   const vanillaResponse = await axios.get(
@@ -50,16 +53,17 @@ test("Generated from / CDX / Binary has ancestors that include the source / Get 
         ancestors: expect.arrayContaining([
           expect.objectContaining({
             relationship: "generates",
-            purl: expect.arrayContaining([cdxSourcePurl])
-          })
-        ])
+            purl: expect.arrayContaining([cdxSourcePurl]),
+          }),
+        ]),
       }),
     ])
   );
 });
 
-test("Generated from / SPDX / Source has descendants that include binaries / Get with pURL", async ({ axios }) => {
-
+test("Generated from / SPDX / Source has descendants that include binaries / Get with pURL", async ({
+  axios,
+}) => {
   const urlEncodedSourcePurl = encodeURIComponent(spdxSourcePurl);
 
   const vanillaResponse = await axios.get(
@@ -74,16 +78,17 @@ test("Generated from / SPDX / Source has descendants that include binaries / Get
         descendants: expect.arrayContaining([
           expect.objectContaining({
             relationship: "generates",
-            purl: expect.arrayContaining([spdxBinaryPurl])
-          })
-        ])
+            purl: expect.arrayContaining([spdxBinaryPurl]),
+          }),
+        ]),
       }),
     ])
   );
 });
 
-test("Generated from / SPDX / Binary has ancestors that include the source / Get with pURL", async ({ axios }) => {
-
+test("Generated from / SPDX / Binary has ancestors that include the source / Get with pURL", async ({
+  axios,
+}) => {
   const urlEncodedBinaryPurl = encodeURIComponent(spdxBinaryPurl);
 
   const vanillaResponse = await axios.get(
@@ -98,9 +103,9 @@ test("Generated from / SPDX / Binary has ancestors that include the source / Get
         ancestors: expect.arrayContaining([
           expect.objectContaining({
             relationship: "generates",
-            purl: expect.arrayContaining([spdxSourcePurl])
-          })
-        ])
+            purl: expect.arrayContaining([spdxSourcePurl]),
+          }),
+        ]),
       }),
     ])
   );
--- tests/api/features/relationship-variant-of.ts
+++ tests/api/features/relationship-variant-of.ts
@@ -3,13 +3,18 @@ import { expect, test } from "../fixtures";
 
 // Effectively tests TC-2052 / TC-2053 - Denote image index to arch image variants relationship.
 
-const cdxBinaryImagePurl = "pkg:oci/ose-console@sha256:c2d69e860b7457eb42f550ba2559a0452ec3e5c9ff6521d758c186266247678e?arch=s390x&os=linux&tag=v4.14.0-202412110104.p0.g350e1ea.assembly.stream.el8";
-const cdxIndexImagePurl = "pkg:oci/openshift-ose-console@sha256:94a0d7feec34600a858c8e383ee0e8d5f4a077f6bbc327dcad8762acfcf40679";
-const spdxBinaryImagePurl = "pkg:oci/ubi9-container@sha256:d4c5d9c980678267b81c3c197a4a0dd206382111c912875a6cdffc6ca319b769?arch=aarch64&repository_url=registry.redhat.io/ubi9&tag=9.2-755.1697625012";
-const spdxIndexImagePurl = "pkg:oci/ubi9-container@sha256:2f168398c538b287fd705519b83cd5b604dc277ef3d9f479c28a2adb4d830a49?repository_url=registry.redhat.io/ubi9&tag=9.2-755.1697625012";
+const cdxBinaryImagePurl =
+  "pkg:oci/ose-console@sha256:c2d69e860b7457eb42f550ba2559a0452ec3e5c9ff6521d758c186266247678e?arch=s390x&os=linux&tag=v4.14.0-202412110104.p0.g350e1ea.assembly.stream.el8";
+const cdxIndexImagePurl =
+  "pkg:oci/openshift-ose-console@sha256:94a0d7feec34600a858c8e383ee0e8d5f4a077f6bbc327dcad8762acfcf40679";
+const spdxBinaryImagePurl =
+  "pkg:oci/ubi9-container@sha256:d4c5d9c980678267b81c3c197a4a0dd206382111c912875a6cdffc6ca319b769?arch=aarch64&repository_url=registry.redhat.io/ubi9&tag=9.2-755.1697625012";
+const spdxIndexImagePurl =
+  "pkg:oci/ubi9-container@sha256:2f168398c538b287fd705519b83cd5b604dc277ef3d9f479c28a2adb4d830a49?repository_url=registry.redhat.io/ubi9&tag=9.2-755.1697625012";
 
-test("Variant of / CDX / Binary image has ancestors that include index image / Get with pURL", async ({ axios }) => {
-
+test("Variant of / CDX / Binary image has ancestors that include index image / Get with pURL", async ({
+  axios,
+}) => {
   const urlEncodedBinaryImagePurl = encodeURIComponent(cdxBinaryImagePurl);
 
   const vanillaResponse = await axios.get(
@@ -23,16 +28,17 @@ test("Variant of / CDX / Binary image has ancestors that include index image / G
         ancestors: expect.arrayContaining([
           expect.objectContaining({
             relationship: "variant",
-            purl: expect.arrayContaining([cdxIndexImagePurl])
-          })
-        ])
+            purl: expect.arrayContaining([cdxIndexImagePurl]),
+          }),
+        ]),
       }),
     ])
   );
 });
 
-test("Variant of / CDX / Index image has descendants that include binary image / Get with pURL", async ({ axios }) => {
-
+test("Variant of / CDX / Index image has descendants that include binary image / Get with pURL", async ({
+  axios,
+}) => {
   const urlEncodedIndexImagePurl = encodeURIComponent(cdxIndexImagePurl);
 
   const vanillaResponse = await axios.get(
@@ -46,16 +52,17 @@ test("Variant of / CDX / Index image has descendants that include binary image /
         descendants: expect.arrayContaining([
           expect.objectContaining({
             relationship: "variant",
-            purl: expect.arrayContaining([cdxBinaryImagePurl])
-          })
-        ])
+            purl: expect.arrayContaining([cdxBinaryImagePurl]),
+          }),
+        ]),
       }),
     ])
   );
 });
 
-test("Variant of / SPDX / Binary image has ancestors that include index image / Get with pURL", async ({ axios }) => {
-
+test("Variant of / SPDX / Binary image has ancestors that include index image / Get with pURL", async ({
+  axios,
+}) => {
   const urlEncodedBinaryImagePurl = encodeURIComponent(spdxBinaryImagePurl);
 
   const vanillaResponse = await axios.get(
@@ -69,16 +76,17 @@ test("Variant of / SPDX / Binary image has ancestors that include index image / 
         ancestors: expect.arrayContaining([
           expect.objectContaining({
             relationship: "variant",
-            purl: expect.arrayContaining([spdxIndexImagePurl])
-          })
-        ])
+            purl: expect.arrayContaining([spdxIndexImagePurl]),
+          }),
+        ]),
       }),
     ])
   );
 });
 
-test("Variant of / SPDX / Index image has descendants that include binary image / Get with pURL", async ({ axios }) => {
-
+test("Variant of / SPDX / Index image has descendants that include binary image / Get with pURL", async ({
+  axios,
+}) => {
   const urlEncodedIndexImagePurl = encodeURIComponent(spdxIndexImagePurl);
 
   const vanillaResponse = await axios.get(
@@ -92,11 +100,10 @@ test("Variant of / SPDX / Index image has descendants that include binary image 
         descendants: expect.arrayContaining([
           expect.objectContaining({
             relationship: "variant",
-            purl: expect.arrayContaining([spdxBinaryImagePurl])
-          })
-        ])
+            purl: expect.arrayContaining([spdxBinaryImagePurl]),
+          }),
+        ]),
       }),
     ])
   );
 });
-
